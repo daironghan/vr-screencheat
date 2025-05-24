6407ce43a55e1ddeb0f0d3605fbf4ae7f7e0959d
daironghan <rongrong910@gmail.com>
2025-05-23 21:26:43 -0700

    Add countdown timer

---
+++ Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MenuNavigation : MonoBehaviour
+{
+    public List<Button> buttons;
+    private int currentIndex = 0;
+
+    void Start()
+    {   
+        if (buttons != null && buttons.Count > 0)
+        {
+            EventSystem.current.SetSelectedGameObject(buttons[currentIndex].gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (buttons == null || buttons.Count == 0) return;
+
+        // Navigate up
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            currentIndex = (currentIndex - 1 + buttons.Count) % buttons.Count;
+            EventSystem.current.SetSelectedGameObject(buttons[currentIndex].gameObject);
+        }
+
+        // Navigate down
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            currentIndex = (currentIndex + 1) % buttons.Count;
+            EventSystem.current.SetSelectedGameObject(buttons[currentIndex].gameObject);
+        }
+
+        // Select current
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            buttons[currentIndex].onClick.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{ 
+    float currentTime = 0f;
+    float startingTime = 60f;
+
+    [SerializeField] TMP_Text countdownText; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+        if(currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+}
